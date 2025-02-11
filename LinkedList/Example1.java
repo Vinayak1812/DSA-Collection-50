2da7dd9b7af673e53a267f1a1cba28987df16f9f
Vinayak Padhyavani <padhyavanivinayak@gmail.com>
2025-02-11 16:36:39 +0530

    Second Commit

---
+++ LinkedList/Example1.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Example1{
+
+    class Node{
+        int data;
+        Node next;
+        Node(){
+
+        }
+        Node(int a){
+            data = a;
+            next = null;
+        }
+        Node(int a, Node next){
+            data = a;
+            this.next = next;
+        }
+
+        public void add(int i){
+            
+        }
+    }
+    public static void main(String[] args){
+        System.out.println("Hello World");
+               
+    }
+}
